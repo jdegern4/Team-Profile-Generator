26d6a3847fc19a89d95825dc2fd1fb0cd714e0b3
Jordan D <degco44@gmail.com>
2022-07-10 21:32:01 -0500

    transfer code from Employee to index

--- index.js
+++ index.js
@@ -4,8 +4,90 @@ const Employee = require('./lib/Employee');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const Manager = require('./lib/Manager');
-let employeeList = [];
+const employeeList = [];
 
-function begin() {
-    init();
-}
+const getInfo = () => {
+    return inquirer.prompt([
+        {
+            type: 'text',
+            name: 'name',
+            message: 'What is the employee name?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log("Please enter the employee's name!");
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'text',
+            name: 'employeeID',
+            message: 'What is the employee ID number?',
+            validate: employeeIDInput => {
+                if (employeeIDInput) {
+                    return true;
+                } else {
+                    console.log("Please enter the employee's ID number!");
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'text',
+            name: 'email',
+            message: "What is the employee's email?",
+            validate: emailInput => {
+                if (emailInput) {
+                    return true;
+                } else {
+                    console.log("Please enter the employee's email!");
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'list',
+            name: 'role',
+            message: "What is the employee's role?",
+            choices: ['Manager', 'Engineer', 'Intern'],
+            validate: roleInput => {
+                if (roleInput) {
+                    return true;
+                } else {
+                    console.log("Please enter the employee's name!");
+                    return false;
+                }
+            }
+        }])
+};
+
+function init() {
+    getInfo()
+    .then(answers => {
+        let employeeCount = 0;
+        if (answers.role === 'Manager') {
+            getManagerInfo();
+        } else if (answers.role === 'Engineer') {
+            getEngineerInfo();
+        } else if (answers.role === 'Intern') {
+            getInternInfo();
+        }
+    });
+};
+
+const getManagerInfo = (employee) => {
+    return inquirer.prompt({
+        type: 'input',
+        name: 'officeNumber',
+        message: 'What is your office number?'
+    })
+    .then(({employee, officeNumber}) => {
+        console.log(employee, officeNumber);
+    });
+};
+
+const anotherEmployee = () => {
+    getInfo();
+};
